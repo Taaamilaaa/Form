2a38d1740b66ed3360fa49983c70b4e0615f7f11
Yudina Tamila <79252896+Taaamilaaa@users.noreply.github.com>
2022-09-17 17:53:28 +0300

    Update toast.js

--- js/toast.js
+++ js/toast.js
@@ -3,11 +3,14 @@ const toastSuccess = Toastify({
     duration: 3000,
     newWindow: true,
     close: false,
-    gravity: 'top', 
-    position: 'right', 
-    stopOnFocus: true, 
+    gravity: 'top',
+    position: 'right',
+    stopOnFocus: true,
     style: {
-        background: 'linear-gradient(to right, #00b09b, #96c93d)',
+        background:
+            'linear-gradient(39deg, rgba(11,233,29,0.7931547619047619) 0%, rgba(207,191,8,0.8743872549019608) 43%, rgba(0,255,184,0.7819502801120448) 100%)',
+        borderRadius: '15px',
+        padding: '50px',
     },
 });
 
@@ -16,13 +19,15 @@ const toastError = Toastify({
     duration: 3000,
     newWindow: true,
     close: false,
-    gravity: 'top', 
-    position: 'right', 
-    stopOnFocus: true, 
+    gravity: 'top',
+    position: 'right',
+    stopOnFocus: true,
     style: {
-        background: 'linear-gradient(to right, red, #96c93d)',
+        background:
+            'linear-gradient(39deg, rgba(233,11,11,0.7931547619047619) 0%, rgba(207,155,8,0.8743872549019608) 43%, rgba(255,252,0,0.7987570028011204) 100%)',
+        borderRadius: '15px',
+        padding: '50px',
     },
 });
 
-
-export {toastSuccess, toastError}
+export { toastSuccess, toastError };
