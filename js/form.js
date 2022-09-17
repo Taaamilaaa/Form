0d104fc7fe61e0363add88efc3dc1d58e599a0eb
Yudina Tamila <79252896+Taaamilaaa@users.noreply.github.com>
2022-09-17 17:34:16 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-// import Notiflix from "notiflix";
+import { toastSuccess, toastError } from './toast.js';
 
 const form = document.querySelector('#main');
 const button = document.querySelector('button');
@@ -16,24 +16,37 @@ function buttonDisabled(e) {
         button.disabled = true;
     } else {
         form.addEventListener('submit', formSubmit);
-
         button.disabled = false;
     }
-}
+};
 
 function formSubmit(e) {
     e.preventDefault();
-
+    
     let error = formValidate(e.target);
 
     if (error === 0) {
-        alert("Ваши данные отправленны в обработку. С вами скоро свяжутся!")
-        // Notiflix.Notify.success('Ваши данные отправленны в обработку. С вами скоро свяжутся!');
-       
+        toastSuccess.showToast();
+
+        reqInputs.forEach(input => {
+            input.value = '';
+        });
     } else {
-        alert("Данные введены не корректно!!!")
-        // Notiflix.Notify.failure('Данные введены не корректно!!!');
-        
+        toastError.showToast();
+    }
+}
+
+reqInputs.forEach(input => {
+    input.addEventListener('input', labelRemove);
+});
+
+function labelRemove(e) {
+    const id = e.target.id;
+    const label = document.querySelector(`[for="${id}"]`);
+
+    if (label.value !== '') {
+        button.disabled = true;
+        label.innerHTML = '';
     }
 }
 
@@ -44,10 +57,8 @@ function formValidate(form) {
     for (let i = 0; i < reqInputs.length; i++) {
         const input = reqInputs[i];
         formRemoveError(input);
-;
-
         if (input.classList.contains('_name')) {
-            let re = /^[a-z\s]+$/i;            
+            let re = /[^\w\s]/u;
 
             if (input.value.match(re) === null) {
                 formAddError(input);
@@ -55,19 +66,19 @@ function formValidate(form) {
             }
         } else if (input.classList.contains('_age')) {
             let re = /^\d+$/;
-            
-            if (input.value.match(re) === null) {
-                formAddError(input);
-                error++;
-            }
-        } else if (input.classList.contains('_phone')) {           
-            let re = /\b[+]?[-0-9\(\) ]{11,20}\b/; 
 
             if (input.value.match(re) === null) {
                 formAddError(input);
                 error++;
             }
-        } 
+        } else if (input.classList.contains('_phone')) {
+            let re = /\b[+]?[-0-9\(\) ]{11,20}\b/;
+
+            if (input.value.match(re) === null) {
+                formAddError(input);
+                error++;
+            }
+        }
     }
 
     return error;
@@ -75,31 +86,18 @@ function formValidate(form) {
 
 function formAddError(input) {
     const label = document.querySelector(`[for="${input.id}"]`);
-    
-    input.classList.add('_error');    
+
+    input.classList.add('_error');
 
     if (input.id === 'name') {
-        label.innerHTML = '*Пишите на кирилице';
+        label.innerHTML = '*Неверный формат имени';
     } else if (input.id === 'age') {
         label.innerHTML = '*Укажите свой возраст';
     } else if (input.id === 'phone') {
-        label.innerHTML = '*Некорректно введен номер';
+        label.innerHTML = '*Неверный формат номера телефона';
     }
 }
 
 function formRemoveError(input) {
     input.classList.remove('_error');
-}
-
-reqInputs.forEach(input => {
-    input.addEventListener('input', labelRemove);
-});
-function labelRemove(e) {
-    const id = e.target.id;
-    const label = document.querySelector(`[for="${id}"]`);
-
-    if (label.value !== '') {
-        button.disabled = true;
-        label.innerHTML = '';
-    }
 }
